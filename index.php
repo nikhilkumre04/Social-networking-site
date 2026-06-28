f76ef0de516b616e28f8e2800d74167d0bc9d6a4
nikhilkumre04 <nikhilkumre1212@gmail.com>
2026-06-28 15:32:19 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Social Network</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1>Welcome to Social Network</h1>
+
+<a href="register.php">Register</a><br><br>
+
+<a href="login.php">Login</a>
+
+</body>
+</html>
